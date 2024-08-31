920319ad9366b56f0843c1330b55a7591dd9650b
WatoLua <32160224+WatoLua@users.noreply.github.com>
2024-08-31 20:02:16 +0200

    Update install_hopper.lua

--- install_hopper.lua
+++ install_hopper.lua
@@ -2,8 +2,8 @@ local githubBaseUrl = "https://raw.githubusercontent.com"
 local username = "WatoLua"
 local defaultBranch = "main"
 local files = {
-    "cc_tweaked-turtle-utils" = {"environment.lua", "inventory.lua"},
-    "cc_tweaked-turtle-hopper" = {"hopper.lua"}
+    ["cc_tweaked-turtle-utils"] = {"environment.lua", "inventory.lua"},
+    ["cc_tweaked-turtle-hopper"] = {"hopper.lua"}
 }
 local function buildUrl(repositoryName, filePath)
     return githubBaseUrl.."/"..username.."/"..repositoryName.."/"..defaultBranch.."/"..filePath
@@ -12,7 +12,7 @@ end
 function main()
     for repo, file in pairs(files) do
         for i=1, #file do
-            shell.run("wget "..buildUrl(repo, file))
+            shell.run("wget "..buildUrl(repo, file[i]))
         end
     end
 end
